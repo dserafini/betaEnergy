c5cff03e49b98fe869efffc3b41762f202c71bcb
dserafini <124274535+dserafini@users.noreply.github.com>
2025-04-22 11:21:29 +0200

    normalize histo with the generated events

--- analysis/drawHisto.C
+++ analysis/drawHisto.C
@@ -19,18 +19,29 @@ void drawHisto(TString file_name = "output0.root") {
         return;
     }
 
+    // prendi parametri dal file
+    TParameter<int>* nEventsParam = (TParameter<int>*)file->Get("nEvents");
+    if (!nEventsParam) {
+        std::cerr << "Errore nel trovare il parametro nEvents!" << std::endl;
+        file->Close();
+        return;
+    }
+    Int_t nEvents = nEventsParam->GetVal();
+    cout << "nEvents = " << nEvents << endl;
+
     // Variabili per leggere i rami
     Double_t eini;
 
     // Collega i rami alle variabili
     tree_channels->SetBranchAddress("eini", &eini);
     
-    Int_t nBinX = 1500;
+    Int_t nBinX = 750;
     Float_t startX = -.5;
     Float_t endX = 1499.5;
     TH1F *hEini = new TH1F("hEini","Initial e- energy",nBinX,startX,endX);
     hEini->GetXaxis()->SetTitle("Energy (keV)");
-    hEini->GetYaxis()->SetTitle("Counts / keV");
+    hEini->GetYaxis()->SetTitle("dN / dE (1/keV)");
+    hEini->GetYaxis()->SetTitleOffset(1.6);
 
     // Loop sugli eventi
     Long64_t nEntries = tree_channels->GetEntries();
@@ -40,6 +51,9 @@ void drawHisto(TString file_name = "output0.root") {
     }
     
 	cout << "hEini integral \t" << hEini->Integral() << endl;
+
+    // Normalizza l'istogramma
+    hEini->Scale(1./nEvents/hEini->GetBinWidth(1),""); // option "nosw2" to avoid statistics error
     
     TCanvas *c2 = new TCanvas("c2","c2");
     c2->cd();
