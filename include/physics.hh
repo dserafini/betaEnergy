1708ae5497916fdf31f13f071e3e38e6eecebaf1
dserafini <124274535+dserafini@users.noreply.github.com>
2025-04-22 11:21:47 +0200

    set cuts

--- include/physics.hh
+++ include/physics.hh
@@ -3,13 +3,15 @@
 #define PHYSICS_HH
 
 #include "G4VModularPhysicsList.hh"
+#include "G4SystemOfUnits.hh"
 
 class MyPhysicsList : public G4VModularPhysicsList
 {
 public:
 	MyPhysicsList();
 	~MyPhysicsList();
-	
+
+	virtual void SetCuts() override;
 	
 };
 
--- src/physics.cc
+++ src/physics.cc
@@ -1,5 +1,7 @@
 #include "physics.hh"
 #include "G4EmStandardPhysics.hh"
+#include "G4EmPenelopePhysics.hh"
+#include "G4EmStandardPhysics_option4.hh"
 #include "G4RadioactiveDecayPhysics.hh"
 
 MyPhysicsList::MyPhysicsList()
@@ -9,8 +11,19 @@ MyPhysicsList::MyPhysicsList()
 	// he advises to use only the physics that is required for your application
 	// the more physics you implement the longer it takes for calculations
 	RegisterPhysics (new G4EmStandardPhysics());
+	// G4EmStandardPhysics G4EmPenelopePhysics G4EmStandardPhysics_option4
 	RegisterPhysics (new G4RadioactiveDecayPhysics());
 }
 
 MyPhysicsList::~MyPhysicsList()
 {}
+
+void MyPhysicsList::SetCuts()
+{
+    // SetCutValue(0.01*mm, "gamma");
+    SetCutValue(1*mm, "e-");
+    // SetCutValue(0.01*mm, "e+");
+
+	// come fatto nell'esempio B3a
+	// G4VUserPhysicsList::SetCuts();
+}
